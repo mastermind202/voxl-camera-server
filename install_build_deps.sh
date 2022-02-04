138ebefedc6aa3c5805814fb97ee52f5068b5378
Alex Gardner <alex.gardner@modalai.com>
2022-02-04 15:20:34 -0800

    update build deps script

--- install_build_deps.sh
+++ install_build_deps.sh
@@ -15,47 +15,82 @@ libmodal-exposure
 libvoxl-cutils"
 
 
-# repo options.Use deb-deb for everything right now since that's all that's up
-# until the first stable SDK 1.0 release is finished
-STABLE="deb [trusted=yes] http://voxl-packages.modalai.com/dev-deb/ ./"
-DEV="deb [trusted=yes] http://voxl-packages.modalai.com/dev-deb/ ./"
+print_usage(){
+    echo ""
+    echo " Install build dependencies from a specified repository."
+    echo " For \"dev\" and \"stable\" repos the packages will be pulled as IPKs"
+    echo " Otherwise debs will be pulled with apt."
+    echo ""
+    echo " Usage:"
+    echo ""
+    echo "  ./install_build_deps.sh dev"
+    echo "        Install from 820 development repo."
+    echo ""
+    echo "  ./install_build_deps.sh stable"
+    echo "        Install from 820 stable repo."
+    echo ""
+    echo "  ./install_build_deps.sh dev-deb"
+    echo "        Install from 865 development repo."
+    echo ""
+    echo ""
+}
+
+# make sure one argument was given
+if [ "$#" -ne 1 ]; then
+    print_usage
+    exit 1
+fi
+REPO=$1
+MODE="DEB"
 
 
-# parse dev or stable option
-if [ "$1" == "stable" ]; then
-	echo "using stable repository"
-	LINE="$STABLE"
-
-elif [ "$1" == "dev" ]; then
-	echo "using development repository"
-	LINE="$DEV"
-
-else
-	echo ""
-	echo "Please specify if the build dependencies should be pulled from"
-	echo "the stable or development modalai opkg package repos."
-	echo "If building the master branch you should specify stable."
-	echo "For development branches please specify dev."
-	echo ""
-	echo "./install_build_deps.sh stable"
-	echo "./install_build_deps.sh dev"
-	echo ""
-	exit 1
+# check for ipk-based repos
+if [ "$REPO" == "dev" ] || [ "$REPO" == "stable" ]; then
+    MODE="IPK"
 fi
 
-# write in the new entry
-DPKG_FILE="/etc/apt/sources.list.d/modalai.list"
-sudo echo "${LINE}" > ${DPKG_FILE}
 
-## make sure we have the latest package index
-## only pull from voxl-packages to save time
-sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/modalai.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+# install deb packages with apt
+if [ "$MODE" == "DEB" ]; then
+    echo "using $REPO DEB repo"
+    # write in the new entry
+    DPKG_FILE="/etc/apt/sources.list.d/modalai.list"
+    LINE="deb [trusted=yes] http://voxl-packages.modalai.com/${REPO}/ ./"
+    sudo echo "${LINE}" > ${DPKG_FILE}
 
-## install the user's list of dependencies
-if [ -n "$DEPS" ]; then
-	echo "installing: "
-	echo $DEPS
-	sudo apt-get install -y $DEPS
+    ## make sure we have the latest package index
+    ## only pull from voxl-packages to save time
+    sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/modalai.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+
+    ## install the user's list of dependencies
+    echo "installing: "
+    echo $DEPS
+    sudo apt-get install -y $DEPS
+
+# install IPK packages with opkg
+else
+    echo "using $REPO IPK repo"
+    OPKG_CONF="/etc/opkg/opkg.conf"
+    # delete any existing repository entries
+    sudo sed -i '/voxl-packages.modalai.com/d' ${OPKG_CONF}
+
+    # write in the new entry
+    sudo echo ${PKG_STRING} >> ${OPKG_CONF}
+    LINE="src/gz ${REPO} http://voxl-packages.modalai.com/${REPO}"
+    sudo echo "$LINE" >> ${OPKG_CONF}
+
+    ## make sure we have the latest package index
+    sudo opkg update
+
+    echo "installing: "
+    echo $DEPS
+
+    # install/update each dependency
+    for i in ${DEPS}; do
+        # this will also update if already installed!
+        sudo opkg install --nodeps $i
+    done
+
 fi
 
 echo ""
