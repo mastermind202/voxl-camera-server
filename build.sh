889300abb141769dc114b369344634496cf4fdc2
Alex Gardner <alex.gardner@modalai.com>
2022-03-25 13:13:55 -0700

    update to use emulator

--- build.sh
+++ build.sh
@@ -3,12 +3,8 @@
 # Copyright (c) 2020 ModalAI, Inc. All rights reserved.
 ################################################################################
 
-TOOLCHAIN32="/opt/cross_toolchain/arm-gnueabi-4.9.toolchain.cmake"
-TOOLCHAIN64="/opt/cross_toolchain/aarch64-gnu-4.9.toolchain.cmake"
-TOOLCHAIN64_865="/opt/cross_toolchain/aarch64-gnu-7.toolchain.cmake"
-
 mkdir -p build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN64_865} ../
+cmake ../
 make -j$(nproc)
 cd ../
